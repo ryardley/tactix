084115176f855913924b02887067978dd4f7d701
ryardley <contact@rudiyardley.com>
2024-10-01 16:17:12 +1000

    Remove unnecessary trait

--- src/tactix.rs
+++ src/tactix.rs
@@ -1,4 +1,3 @@
-
 use std::marker::PhantomData;
 
 use async_trait::async_trait;
@@ -25,7 +24,6 @@ pub trait EnvelopeApi<A: Actor> {
     fn handle(&mut self, act: &mut A);
 }
 
-// 
 pub struct Envelope<A>(pub Box<dyn EnvelopeApi<A> + Send + 'static>);
 impl<A> Envelope<A> {
     pub fn new(inner: Box<dyn EnvelopeApi<A> + Send + 'static>) -> Self {
@@ -73,51 +71,55 @@ impl<A: Actor> Addr<A> {
     fn new(tx: mpsc::Sender<Envelope<A>>) -> Self {
         Self { tx }
     }
-}
 
-#[async_trait]
-pub trait AddrSend<A, M>
-where
-    A: Actor + Handler<M>,
-    M: Message + Send,
-{
-    fn do_send(&self, msg: M);
-    async fn send(&self, msg: M) -> Result<M::Response, String>;
-}
-
-#[async_trait]
-impl<A, M> AddrSend<A, M> for Addr<A>
-where
-    A: Actor + Handler<M> + 'static,
-    M: Message + Send + 'static,
-{
-    fn do_send(&self, msg: M) {
-        // Setup the envelope without a transmitter
-        let env: Envelope<A> = Envelope::new(Box::new(EnvelopeInner {
-            msg: Some(msg),
-            tx: None,
-        }));
-
+    fn send_env(&self, env: Envelope<A>) {
         let tx = self.tx.clone();
         tokio::spawn(async move {
             let _ = tx.send(env).await;
         });
     }
+}
 
-    async fn send(&self, msg: M) -> Result<M::Response, String> {
+trait ToEnvelope<A, M>
+where
+    A: Actor,
+    M: Message + Send,
+{
+    fn to_env(&self, msg: Option<M>, tx: Option<Sender<M::Response>>) -> Envelope<A>;
+}
+
+impl<A, M> ToEnvelope<A, M> for Addr<A>
+where
+    A: Actor + Handler<M>,
+    M: Message + Send + 'static,
+{
+    fn to_env(&self, msg: Option<M>, tx: Option<Sender<M::Response>>) -> Envelope<A> {
+        Envelope::new(Box::new(EnvelopeInner { msg, tx }))
+    }
+}
+
+impl<A: Actor> Addr<A> {
+    pub fn do_send<M>(&self, msg: M)
+    where
+        M: Message + Send + 'static,
+        A: Actor + Handler<M>,
+    {
+        // Setup the envelope without a transmitter
+        let env = self.to_env(Some(msg), None);
+        self.send_env(env);
+    }
+
+    pub async fn send<M>(&self, msg: M) -> Result<M::Response, String>
+    where
+        M: Message + Send + 'static,
+        A: Actor + Handler<M>,
+    {
         // make a oneshot
         let (tx, rx) = oneshot::channel::<M::Response>();
 
         // pass it to the envelope
-        let env: Envelope<A> = Envelope::new(Box::new(EnvelopeInner {
-            msg: Some(msg),
-            tx: Some(tx),
-        }));
-        
-        let tx = self.tx.clone();
-        tokio::spawn(async move {
-            let _ = tx.send(env).await;
-        });
+        let env = self.to_env(Some(msg), Some(tx));
+        self.send_env(env);
 
         // receive on the oneshot
         Ok(rx.await.map_err(|_| "yikes".to_string())?)
@@ -163,4 +165,3 @@ where
         addr
     }
 }
-
